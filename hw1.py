7200d239a09a1aa6f557a05543338d1eb57ad25e
Alexander <bilyakalexxx@gmail.com>
2019-11-14 01:12:12 +0200

    try
    for feedb

--- hw1.py
+++ hw1.py
@@ -1,19 +1,19 @@
 def have_objects_same_value(first, second):
 
     print (first == second)
-    return
+
 have_objects_same_value (1,1)
 
 
 def is_objects_same_type(first, second):
 
     print (type(first) == type(second))
-    return
+
 is_objects_same_type (3,5)
 
 
 def is_objects_the_same(first, second):
 
     print (first == second)
-    return
+
 is_objects_the_same (2,1)
