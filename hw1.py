66e2e40225e579f8938855a74c9b18ec88d20422
Alexander <bilyakalexxx@gmail.com>
2019-11-13 23:02:02 +0200

    hw1
    created a py file

---
+++ hw1.py
@@ -0,0 +1,35 @@
+"""
+This is a list of functions that should be completed.
+"""
+
+def have_objects_same_value(first, second):
+
+    print (first == second)
+    return
+
+have_objects_same_value (1,1)
+
+    # """
+    # If @first and @second has same value should return True
+    # In another case should return False
+    # """
+    # pass
+
+def is_objects_same_type(first, second):
+    """
+    If @first and @second has same type should return True
+    In another case should return False
+    """
+    print (type(first) == type(second))
+    return
+
+is_objects_same_type (3,5)
+
+
+# val1 = 'a'
+# val2 = 'a'
+
+# print (type(val1) == type(val2))
+
+    # pass
+    
